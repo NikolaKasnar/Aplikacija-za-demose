35b863285c437d72721109e3b5e815d153f38a82
zacaxzu <zeljka.baca1@gmail.com>
2024-04-18 10:04:15 +0200

    navigation bar and login view added

---
+++ library/view/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css" />
+</head>
+
+<body>
+    <?php include('navigationbar.php') ?>
+    
+    <form action="action_page.php" method="post">
+        <div class="imgcontainer">
+            <img src="img_avatar2.png" alt="Avatar" class="avatar">
+        </div>
+
+        <div class="container">
+            <label for="uname"><b>Korisničko ime</b></label><br>
+            <input type="text" placeholder="Unesite korisničko ime" name="uname" required>
+            <br>
+            <label for="psw"><b>Lozinka</b></label><br>
+            <input type="password" placeholder="Unesite lozinku" name="psw" required>
+            <br>
+            <button class="submitbtn" type="submit">Login</button>
+            <br>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Zapamti me
+            </label>
+        </div>
+
+        <div class="container" style="background-color:#f1f1f1">
+            <!--<button type="button" class="cancelbtn">Odustani</button>-->
+            <span class="psw">Zaboravili ste <a href="#">lozinku?</a></span>
+        </div>
+    </form>
+
+</body>
+
+</html>
---
+++ library/view/navigationbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="navigationbar.css" />
+</head>
+
+<body>
+    <div class="topnav">
+        <a class="active" href="#login">Login</a>
+        <a href="#postani_demos">Postani demos</a>
+        <a href="#upute">Opis posla</a>
+        <a href="#rezervacije">Rezervacije</a>
+    </div>
+</body>
+
+</html>
