fa532053137d9332911a34f558c13ddb4850dab9
DominikH123 <147082218+DominikH123@users.noreply.github.com>
2024-05-22 21:59:11 +0200

    novi demos prikaz

--- library/controller/adminpostavkeController.class.php
+++ library/controller/adminpostavkeController.class.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../model/loginservice.class.php';
 require_once __DIR__ . '/../model/libraryservice.class.php';
 require_once __DIR__ . '/../model/userservice.class.php';
 require_once __DIR__ . '/../model/user.class.php';
+require_once __DIR__ . '/../model/adminpostavkeservice.class.php';
+require_once __DIR__ . '/../model/info.class.php';
 
 class AdminpostavkeController
 {
@@ -13,6 +15,27 @@ class AdminpostavkeController
 
         $user=$st->getuser($_COOKIE['username']);
         require_once __DIR__ . '/../view/admin-postavke/postavke_html.php';
+    }
+
+    //prikazuje podatke o studentima koji su ispunili formu postani demos
+    public function noviDemos()
+    {
+        $broj_novih = 0;
+        $st=new UserService();
+        $user=$st->getuser($_COOKIE['username']);
+
+        $ap = new AdminPostavkeService();
+        $infoList = $ap->dohvatiInfo();
+        $terminList = [];
+        foreach($infoList as $info){
+            $terminList[$broj_novih] = $ap->dohvatiTermin($info->mail_faks);
+            $broj_novih++;
+        }
+
+
+
+        require_once __DIR__ . '/../view/admin-postavke/novidemos_html.php';
+
     }
 
 };
