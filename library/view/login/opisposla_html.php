b07889bc88a106af0fcc2c3c9cc3eef70f01463a
nikola <nkasnar@gmail.com>
2024-05-25 00:03:21 +0200

    Dodan opis posla dezuranja

--- library/view/login/opisposla_html.php
+++ library/view/login/opisposla_html.php
@@ -6,7 +6,34 @@
 
         <h1>Zanima te posao demos? Ovdje možeš proćitati čime se bavimo! :)</h1>
 
-        
+        <p>Posao se sastoji od dvije dužnosti: dežuranja praktikuma i snimanja predavanja. Ukratko slijedi opis obje dužnosti.</p>
+
+        <h2>Dežuranja</h2>
+
+        <ol class="opis-posla">
+            <li>
+                Demonstrator dežura u praktikumima kada su oni otvoreni za učenje studentima te je tamo dužan biti od početka do kraja svog termina
+            </li>
+            <br>
+            <li>
+                Za vrijeme svog dežuranja demonstrator pazi na mir i red u praktikumu te na zahtjev studenata printa najviše 20 listova papira po osobi
+            </li>
+            <br>
+            <li>
+                Ako se dogodi da računalo ili printer ne radi dužan je obavijestiti računski centar lociran u podrumu fakulteta ili kontaktirati glavnog demonstratora
+            </li>
+            <br>
+            <li>
+                Po završetku svog termina demonstrator je dužan jesu li sva računala u praktikumu u kojemu dežura funkcionalna te ukoliko nisu treba obavijestiti računski centar lociran u podrumu fakulteta ili kontaktirati glavnog demonstratora (ukoliko je demonstrator zadužen za zadnji termin u danu dužan je nakon provjere ugasiti sva računala)
+            </li>
+        </ol>
+
+        <p>Primjer radnog mjesta:</p>
+
+        <div class="imgcontainer-opis-posla">
+            <img src="./view/images/praktikum4.jpeg" alt="pr4" class="pr">
+        </div>
+    
     
     </div>
 
