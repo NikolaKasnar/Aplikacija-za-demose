e95e480be8fc33797327815d730df537d4e506ef
DominikH123 <147082218+DominikH123@users.noreply.github.com>
2024-07-02 15:12:36 +0200

    slanje mailova demosima o novoj obavijesti

--- library/controller/obavijestiController.class.php
+++ library/controller/obavijestiController.class.php
@@ -5,7 +5,8 @@ class ObavijestiController
 {
     public function index()
     {
-        if(isset($_COOKIE['ovlasti']) && $_COOKIE['ovlasti'] === '0'){
+        if(isset($_COOKIE['ovlasti']) && $_COOKIE['ovlasti'] === '0')
+        {
             //ovisno o tome je li admin po ovlastima (0)
 
             $st=new ObavijestiService();
@@ -13,7 +14,8 @@ class ObavijestiController
 
             require_once __DIR__ . '/../view/obavijesti/obavijesti-admin_html.php';
         }
-        else{
+        else
+        {
             //ili demonstrator (1)
 
             $st=new ObavijestiService();
@@ -26,23 +28,65 @@ class ObavijestiController
     public function obradiObavijest(){
         $st=new ObavijestiService();
         $notifications=$st->dohvatiObavijesti();
-        if(isset($_COOKIE['ovlasti']) && $_COOKIE['ovlasti'] === '1'){
+        if(isset($_COOKIE['ovlasti']) && $_COOKIE['ovlasti'] === '1')
+        {
             //demonstrator (1)
             require_once __DIR__ . '/../view/obavijesti/obavijesti-demos_html.php';
         }
-        else{
+        else
+        {
             //admin po ovlastima (0)
             //dalje sad provjeravamo unos nove obavijesti
-            if(isset($_POST['btn'])){
+            if(isset($_POST['btn']))
+            {
                 //moramo obraditi primljene podatke i ubaciti u tablicu!
                 //u $_POST['naslov_poruke'] i $_POST['tijelo_poruke'] nalazi se ono što trebamo za ubačaj u tablicu
+
+                $naslov_obavijesti = $_POST['naslov_poruke'];
+                $tijelo_obavijesti = $_POST['tijelo_poruke'];
+                
+                $online_admin = $_COOKIE['username'];
+
                 $os = new ObavijestiService();
-                $os->pohraniObavijest($_POST['naslov_poruke'], $_POST['tijelo_poruke']);
-                require_once __DIR__ . '/../view/obavijesti/obavijesti-admin_html.php';
+                $os->pohraniObavijest($naslov_obavijesti, $tijelo_obavijesti);
+
+                //treba poslati obavijest svim demosima (znaci svima koji imaju ovlast === 1)
+                //prvo dohvatimo demose i onda im saljemo!
+                //u demose ce biti dohvacen njihov e-mail
+                $demosi = array();
+                $demosi = $os->dohvatiDemose();
+
+                //dohvacamo ime i prezime admina kako bi napisali na kraju maila tko je to posalo :)
+                $trenutni_admin = $os->dohvatiTrenutnogAdmina($online_admin);
+
+                if ($trenutni_admin !== null) 
+                {
+                    $ime_admin = $trenutni_admin['ime'];
+                    $prezime_admin = $trenutni_admin['prezime'];
+                } 
+                else 
+                {
+                    echo "Administrator nije pronađen.";
+                }
+
+                $rez_poslano = $os->posaljiObavijestDemosima($naslov_obavijesti, $tijelo_obavijesti, $demosi, $ime_admin, $prezime_admin);
+
+                //provjera s porukom
+                if($rez_poslano)
+                {
+                    header("Location: index.php?rt=obavijesti/index"); //da se zaustavi ponovni unos u tablicu kod osvjezavanja stranice
+                }
+                else
+                {
+                    header("Location: index.php?rt=obavijesti/index"); //da se zaustavi ponovni unos u tablicu kod osvjezavanja stranice
+                    exit();
+                }
+
+                header("Location: index.php?rt=obavijesti/index"); //da se zaustavi ponovni unos u tablicu kod osvjezavanja stranice
+                exit(); 
             }
-            else{
-                //u slucaju da je pristupljeno direktno putem linka! bez pritiska gumba i ispunjene forme
-                //ne radi ništa i samo ga preusmjeri ponovno na istu stranicu
+            else
+            {
                 require_once __DIR__ . '/../view/obavijesti/obavijesti-admin_html.php';
             }
         }
--- library/model/obavijestiservice.class.php
+++ library/model/obavijestiservice.class.php
@@ -36,6 +36,76 @@ class ObavijestiService
         catch(PDOException $e) {exit('PDO error ' . $e->getMessage());}
         return;
     }
+    
+    //funkcija koja vraca ime i prezime admina
+    public function dohvatiTrenutnogAdmina($online_admin)
+    {
+        try
+        {
+            $db = DB::getConnection();
+            $st = $db->prepare('SELECT ime, prezime FROM demosi WHERE username=:online_admin');
+            $st->execute([':online_admin' => $online_admin]);
+
+            $row = $st->fetch(PDO::FETCH_ASSOC);
+
+            if ($row) 
+            {
+                return $row;
+            } 
+            else 
+            {
+                return null;
+            }
+        }
+        catch(PDOException $e)
+        { 
+            exit('PDO error ' . $e->getMessage()); 
+        }
+    }
+
+    //funkcijom vracamo polje e-mailova svih demosa
+    public function dohvatiDemose(){
+        //spajanje na bazu
+        try
+		{
+			$db = DB::getConnection();
+			$st = $db->prepare( 'SELECT email FROM demosi WHERE ovlasti=1' );
+			$st->execute();
+		}
+		catch(PDOException $e) 
+        { 
+            exit('PDO error ' . $e->getMessage()); 
+        }
+
+		$arrofmail = array();
+		while($row = $st->fetch())
+		{
+			$arrofmail[] = $row['email'];
+		}
+		return $arrofmail;
+    }
+
+    //funkcija za slanje maila demonstratorima
+    public function posaljiObavijestDemosima($naslov_obavijesti, $tijelo_obavijesti, $demosi, $ime_admin, $prezime_admin){
+        $subject = $naslov_obavijesti;
+        $message = 'Poštovani demostratore,' . "\n" . $tijelo_obavijesti;
+
+        $message .= "\n" . "\n" . 'Lijep pozdrav,' . "\n" . $ime_admin . ' ' . $prezime_admin;
+        $headers  = 'From: rp2@studenti.math.hr' . "\r\n" .
+                    'Reply-To: rp2@studenti.math.hr' . "\r\n" .
+                    'X-Mailer: PHP/' . phpversion() . "\r\n" .
+                    'Content-Type: text/plain; charset=UTF-8' . "\r\n";
+
+        // Posaljemo mail svim demonstratorima
+        $isOK=1;
+        foreach($demosi as $demos)
+        {
+            $isOK=mail($demos, $subject, $message, $headers);
+        }
+        if( !$isOK )
+			exit( 'Greška: ne mogu poslati mail. (Pokrenite na rp2 serveru.)' );
+        return 1;
+    }
 
 };
 
