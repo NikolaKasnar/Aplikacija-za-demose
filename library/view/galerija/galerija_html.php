9367722cd878f1dd9a3999849ca5a1fd68380bb5
DominikH123 <147082218+DominikH123@users.noreply.github.com>
2024-07-03 09:42:09 +0200

    jos veci upgrade stila galerije i forme za baratnje istom

--- library/view/galerija/galerija_html.php
+++ library/view/galerija/galerija_html.php
@@ -9,26 +9,31 @@
 <?php require_once __DIR__ . '/../_header.php'; ?>
 
 
-<h1>Dobrodošli u galeriju &#128513</h1>
+<h1 id="naslov-galerija">&#128247 Dobrodošli u galeriju &#128247</h1>
 
 
 <!-- slijedi kod za prikaz uploadanih slika -->
 <?php
 $files = glob("view/images/slike_galerija/*.{jpg,jpeg,png}", GLOB_BRACE);
-echo '<div class="gallery-container">';
 foreach ($files as $image) {
-    echo '<div class="gallery">';
-    echo '<img src="' . $image . '" alt="Random image" />';
-    echo '<div class="capiton">' . basename($image) . '</div>';
+    echo '<div class="galerija-responsive">';
+    echo '<div class="galerija">';
+    echo '<img src="' . $image . '" alt="Cinque Terre">';
+    echo '<div class="galerija-desc">' . basename($image) . '</div>';
+    echo '</div>';
     echo '</div>';
 }
-echo '</div>';
+echo '<div class="clearfix"></div>';
 ?>
 
 <br>
-<hr>
 
-<h3><strong>Info: </strong>Ako želite da i Vaša slika bude zabilježena u galeriji molimo javite se voditelju stranice</h3>
+<div class="galerija-alert">
+  <span class="galerija-closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+  Ako želite da i Vaša slika bude zabilježena u galeriji molimo javite se voditeljima stranice.
+</div>
+
+<!-- <h3><strong>Info: </strong>Ako želite da i Vaša slika bude zabilježena u galeriji molimo javite se voditelju stranice</h3> -->
 
 <br>
 </body>
